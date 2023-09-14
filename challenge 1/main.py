b81f4196292207d200479892d8f51c467105a097
shimna2004 <144349286+shimna2004@users.noreply.github.com>
2023-09-14 11:15:04 +0530

    Add files via upload

---
+++ challenge 1/main.py
@@ -0,0 +1,18 @@
+
+
+def factorial(x):
+    
+
+    if x == 1:
+        return 1
+    else:
+        
+        return (x * factorial(x-1))
+
+
+
+num = 5
+
+
+result = factorial(num)
+print("The factorial of", num, "is", result)
